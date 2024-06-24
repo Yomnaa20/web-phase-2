7106bbc4ec2d11ae43b48830d9eb5dab1e4bbb91
Moatasem <167562293+Moatasseem@users.noreply.github.com>
2024-06-24 07:08:06 +0300

    Delete models/surveySchema.js

--- models/surveySchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const surveySchema = new mongoose.Schema({
-    quesId: { type: Number, required: true},
-    question: { type: String, required: true }
-}, { collection: 'questions' });
-
-module.exports = mongoose.model('Survey', surveySchema);
